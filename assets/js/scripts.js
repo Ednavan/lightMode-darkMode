bc8f38ac43754f77d0a0180d056840115e80f426
Ednavan <ednavanfisico@gmail.com>
2022-01-14 14:59:23 -0300

    create effect darkmode and light mode

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,34 @@
 
 function changeMode(){
     changeClasses();
+    changeText();
 }
 
 function changeClasses(){
-    btn.classList.toggle('dark-mode');
-    h1.classList.toggle('dark-mode');
-    body.classList.toggle('dark-mode');
-    footer.classList.toggle('dark-mode');
+    btn.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass);     //ao clicar no botao pode-se observar na inspeção que a class dark-mode é adicionada.
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
 }
 
+/**@param changeText funcao que muda o texto de acordo com o click do button */
+function changeText(){
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+
+    /**@param body seu uso aqui é porque ele comanda toda a página */
+    if(body.classList.contains(darkModeClass)){  //contains vai verificar dentro da classLista de botão se existe alguma coisa, ou seja, se existe o  'dark-mode'
+        btn.innerHTML = lightMode
+        h1.innerHTML = darkMode;
+        return;  //com esse retorno, faz com que saia da função
+    }     
+    
+    btn.innerHTML = darkMode;
+    h1.innerHTML = lightMode; 
+}
+
+const darkModeClass = 'dark-mode'
 const btn = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
 const body = document.getElementsByTagName('body')[0]; //Estou pegando pela própria tag no caso body
